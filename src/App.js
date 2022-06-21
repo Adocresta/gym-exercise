591febcf33a12dff9666de0bb0c879c46aa58e40
Onur <znygaga004@gmail.com>
2022-06-22 00:40:48 +0300

    File & Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import NavBar from "./assets/components/NavBar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Footer from "./assets/components/Footer";
+
 const App = () => {
   return (
-    <div>
-      <h1>REACT</h1>
-    </div>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/execise/:id" element={<ExerciseDetail />}></Route>
+      </Routes>
+      <Footer />
+    </Box>
   );
 };
 
